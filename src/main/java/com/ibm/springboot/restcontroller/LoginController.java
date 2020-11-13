61f8b02e1f564326fe8a196ed006aebade51f1d2
Kliurat <k1971367778@163.com>
2020-11-13 19:20:46 +0800

    测试HP电脑Git

---
+++ src/main/java/com/ibm/springboot/restcontroller/LoginController.java
@@ -0,0 +1,51 @@
+package com.ibm.springboot.restcontroller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.springboot.entity.CommonResult;
+
+@RestController
+
+public class LoginController {
+
+	@PostMapping("/login")
+	public CommonResult<String> login(@RequestParam("username") String username,
+			@RequestParam("password") String password) {
+		int status = 200;
+		String msg = null;
+
+		// 用户名密码判空验证在前端编写
+//		if(username == null || password == null || 
+//				"".equals(username.trim()) || 
+//				"".equals(password.trim())) {
+//			
+//			if (username == null || "".equals(username.trim())) {
+//				status = 201;
+//				msg = "用户名不能为空";
+//			}
+//
+//			if (password == null || "".equals(password.trim())) {
+//				status = 201;
+//				msg = "密码不能为空";
+//			}
+//		}else {
+//			if (!) {
+//				
+//			}
+//		}
+
+		if (username.equals(password)) {
+			status = 200;
+			msg = "登陆成功";
+		} else {
+			status = 201;
+			msg = "用户名或密码错误";
+		}
+
+//		return msg;
+
+		return new CommonResult<String>(status, msg, null);
+	}
+}
