2c0a62e34f53277fc293243496a4617f89074498
Kliurat <k1971367778@163.com>
2020-11-17 11:21:53 +0800

    CommonResult添加了get、set方法

--- src/main/java/com/ibm/springboot/entity/CommonResult.java
+++ src/main/java/com/ibm/springboot/entity/CommonResult.java
@@ -19,4 +19,41 @@ public class CommonResult<T> {
 	// Response数据
 	private T data;
 
+	public CommonResult() {
+		super();
+	}
+
+	public CommonResult(Integer status, String msg, T data) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.data = data;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	
+
 }
