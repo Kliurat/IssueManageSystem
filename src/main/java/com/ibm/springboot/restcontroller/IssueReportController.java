e3a7333bc6fb370304ae55edf5954e5227b356bd
Kliurat <k1971367778@163.com>
2020-11-20 21:07:23 +0800

    IssueReport的模糊查询

--- src/main/java/com/ibm/springboot/restcontroller/IssueReportController.java
+++ src/main/java/com/ibm/springboot/restcontroller/IssueReportController.java
@@ -21,25 +21,33 @@ public class IssueReportController {
 	@Resource
 	IssueReportService iService;
 
-	@GetMapping("")
-	public CommonResult getAll(@RequestParam(value = "loginId", required = false) String loginId,
+	@RequestMapping("")
+	public CommonResult getAll(@RequestParam(value = "loginID", required = false) String userId,
 			@RequestParam(value = "username", required = false) String username
 //			@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum
 	) {
 
 //		System.out.println("IssueReportController=>getAll：PageNum=" + pageNum);
-		System.out.println("loginId:" + loginId);
+		System.out.println("userId:" + userId);
 		System.out.println("username:" + username);
 
 		int status = 200;
 		String msg = "查询成功";
+		
+		if(username == "") {
+			username = null;
+		}
+		if (userId == "") {
+			userId = null;
+		}
 
 //		PageHelper.startPage(pageNum, ConstantUtil.PAGE_SIZE_5);
 
-		List<IssueReport> list = iService.findAll(loginId, username);
+		List<IssueReport> list = iService.findAll(userId, username);
 
 		if (list == null) {
 			list = new ArrayList<IssueReport>();
+			System.out.println("list为null");
 		}
 
 //		PageInfo<IssueReport> page = new PageInfo<IssueReport>(list);
@@ -48,6 +56,10 @@ public class IssueReportController {
 //				page.getNavigateLastPage(), page.getPageSize(), page.getTotal(), list);
 //
 //		return new CommonResult<MyPageInfo<IssueReport>>(200, msg, dataPage);
+		
+		for (IssueReport issueReport : list) {
+			System.out.println(issueReport);
+		}
 
 		return new CommonResult<List<IssueReport>>(status, msg, list);
 	}
